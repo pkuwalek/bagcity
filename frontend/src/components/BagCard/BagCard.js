3f5336ae4ab6d9a8a2db59076de08dc98878bba6
Paula Kuwałek <paula.kuwalek@gmail.com>
2022-04-10 18:08:38 +0200

    add clickable links to single bag page

--- frontend/src/components/BagCard/BagCard.js
+++ frontend/src/components/BagCard/BagCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
 const BagCard = (props) => {
-    console.log(props);
     return (
         <Card style={{ width: '18rem' }}>
         <Card.Body>
+            <Link to={`/bags/${props.bags.bag_id}`}>
             <Card.Title>Name: {props.bags.bag_name}</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">Price: {props.bags.price}$</Card.Subtitle>
-            <Card.Text>
-            Brand: {props.bags.brands.brand_name}
-            </Card.Text>
+            <Card.Text>Brand: {props.bags.brands.brand_name}</Card.Text>
+            </Link>
         </Card.Body>
         </Card>
     )
