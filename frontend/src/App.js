1292c911fe81a1afe7a084599b450e55f2f7e2ca
Paula Kuwałek <paula.kuwalek@gmail.com>
2022-07-03 17:23:47 +0200

    create registration form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.scss';
 import BagsPage from './components/bagsPage/bagsPage';
 import BagPage from './components/bagPage/bagPage';
+import LoginPage from './components/loginPage/loginPage';
+import RegistrationPage from './components/registrationPage/registrationPage';
 import {
   BrowserRouter,
   Route,
@@ -15,6 +17,8 @@ function App() {
           <Routes>
             <Route path='/bags' element={<BagsPage />}></Route>
             <Route path='/bags/:id' element={<BagPage />}></Route>
+            <Route path='/login' element={<LoginPage />}></Route>
+            <Route path='/register' element={<RegistrationPage />}></Route>
           </Routes>
         </BrowserRouter>
       </header>
---
+++ frontend/src/components/registrationPage/registrationPage.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import './registrationPage.scss';
+
+const RegistrationPage = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            password: ''
+        },
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .required('Required'),
+            email: Yup.string()
+                .email('Invalid email address')
+                .required('Required'),
+            password: Yup.string()
+                .required('Required')
+                .min(8, 'Password should be 8 chars minimum'),
+        }),
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+ 
+    return (
+    <>
+    <h2>Register</h2>
+    <Form onSubmit={formik.handleSubmit}>
+        <FloatingLabel controlId="floatingInput" label="Name" className="mb-3">
+            <Form.Control
+            id="name"
+            type="text"
+            placeholder="Name"
+            {...formik.getFieldProps('name')}
+            />
+        {formik.touched.name && formik.errors.name ? (<div>{formik.errors.name}</div>
+        ) : null}
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
+            <Form.Control
+            id="email"
+            type="email"
+            placeholder="name@example.com"
+            {...formik.getFieldProps('email')}
+            />
+        {formik.touched.email && formik.errors.email ? (<div>{formik.errors.email}</div>
+        ) : null}
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingPassword" label="Password" className="mb-3">
+            <Form.Control
+            id="password"
+            type="password"
+            placeholder="Password"
+            {...formik.getFieldProps('password')}
+            />
+        {formik.touched.password && formik.errors.password ? (<div>{formik.errors.password}</div>
+        ) : null}
+        </FloatingLabel>
+        <Button variant="primary" type="submit">
+            Sign up
+        </Button>
+    </Form>
+    </>
+    )
+};
+export default RegistrationPage;
