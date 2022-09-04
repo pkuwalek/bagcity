dcb6cbc5ac987dd2dfb3fea9b0858d8150de9c82
Paula Kuwałek <paula.kuwalek@gmail.com>
2022-09-04 19:23:48 +0200

    add user name to usersPage

--- frontend/src/components/usersPage/usersPage.js
+++ frontend/src/components/usersPage/usersPage.js
@@ -1,9 +1,10 @@
-import React, { useCallback, useContext, useEffect } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../context/userContext';
 import { getUserDetails } from '../../sources/users';
 
 const UsersPage = () => {
   const [userContext, setUserContext] = useContext(UserContext);
+  const [userName, setUserName] = useState('');
 
   const getDetails = useCallback(() => {
     getUserDetails(userContext.token).then(async (response) => {
@@ -24,6 +25,9 @@ const UsersPage = () => {
     if (!userContext.details && userContext.token) {
       getDetails();
     }
+    if (userContext.details) {
+      setUserName(userContext.details.user_name);
+    }
   }, [userContext.details, userContext.token, getDetails]);
 
   return userContext.details === null ? (
@@ -31,7 +35,7 @@ const UsersPage = () => {
   ) : (
     <>
       <h1>Greetings user</h1>
-      <h3>{JSON.stringify(userContext.details)}</h3>
+      <h3>{userName}</h3>
     </>
   );
 };
