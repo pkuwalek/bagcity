26714e9e4d5dbab40021e899435bafe6f2aaeedc
Paula Kuwałek <paula.kuwalek@gmail.com>
2022-10-11 19:07:50 +0200

    add usersPage behaviour

--- frontend/src/components/usersPage/usersPage.js
+++ frontend/src/components/usersPage/usersPage.js
@@ -1,21 +1,26 @@
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
+import Spinner from 'react-bootstrap/Spinner';
 import { UserContext } from '../../context/userContext';
 import { getUserDetails } from '../../sources/users';
+import UsersPageContent from './usersPageContent';
 
 const UsersPage = () => {
   const [userContext, setUserContext] = useContext(UserContext);
   const [userName, setUserName] = useState('');
+  const [isReady, setIsReady] = useState('waiting');
 
   const getDetails = useCallback(() => {
     getUserDetails(userContext.token).then(async (response) => {
       if (response.ok) {
         const data = await response.json();
         setUserContext((oldValues) => {
+          setIsReady('accepted');
           return { ...oldValues, details: data };
         });
       } else {
         setUserContext((oldValues) => {
+          setIsReady('rejected');
           return { ...oldValues, details: null };
         });
       }
@@ -23,22 +28,25 @@ const UsersPage = () => {
   }, [setUserContext, userContext.token]);
 
   useEffect(() => {
-    if (!userContext.details && userContext.token) {
+    if (userContext.token === null) {
+      setIsReady('rejected');
+    } else if (!userContext.details && userContext.token) {
       getDetails();
-    }
-    if (userContext.details) {
+    } else if (userContext.details) {
       setUserName(userContext.details.user_name);
     }
   }, [userContext.details, userContext.token, getDetails]);
 
-  return userContext.details === undefined ? (
-    <Navigate replace to="/login" />
-  ) : (
-    <>
-      <h1>Greetings user</h1>
-      <h3>{userName}</h3>
-    </>
-  );
+  let currentContent;
+  if (isReady === 'rejected') {
+    currentContent = <Navigate replace to="/login" />;
+  } else if (isReady === 'accepted') {
+    currentContent = <UsersPageContent />;
+  } else {
+    currentContent = <Spinner animation="border" variant="secondary" />;
+  }
+
+  return <>{currentContent}</>;
 };
 
 export default UsersPage;
---
+++ frontend/src/components/usersPage/usersPageContent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UsersPageContent = () => {
+  return <h2>Welcome to users page!</h2>;
+};
+
+export default UsersPageContent;
